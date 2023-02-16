5f0b07a4af108997ed8cf1104d0060827d417d22
shib1991 <54975465+shib1991@users.noreply.github.com>
2023-02-16 17:59:33 +0300

    Сохранил для себя

---
+++ js/weather.js
@@ -0,0 +1,11 @@
+const SearchButton = document.querySelector('#Main_search-bar_search-img');
+const SearchLocation = document.querySelector('#Main_search-bar_input');
+
+SearchButton.addEventListener('onclick',addLocation);
+
+function addLocation(){
+    console.log('Hello!');
+
+
+}
+
