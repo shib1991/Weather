e4259099940046333327b3b7e1f6cd355f8e0654
shib1991 <54975465+shib1991@users.noreply.github.com>
2023-02-19 22:00:55 +0300

    Сохраняю для себя

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,27 @@
-const SearchButton = document.querySelector('#Main_search-bar_search-img');
-const SearchLocation = document.querySelector('#Main_search-bar_input');
-
-SearchButton.addEventListener('onclick',addLocation);
-
-function addLocation(){
-    console.log('Hello!');
+const form = document.querySelector('.Main_search-bar');
+const searchButton = document.querySelector('.Main_search-bar_search-img');
+const searchLocationInput = document.querySelector('.Main_search-bar_input')
 
 
+/* searchButton.addEventListener('click', weather(searchLocationInput.value));
+searchButton.addEventListener('submit', );
+ */
+
+
+function weather(cityName) {
+
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const url = (`${serverUrl}?q=${cityName}&appid=${apiKey}`);
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+
+        })
+        .then((data) =>
+            console.log(Math.round( data.main.temp - 273))); // Температура 
 }
 
+
+weather('London');
