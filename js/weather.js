9afc5c3dc6accf40c4d83514bca3624fc027a407
shib1991 <54975465+shib1991@users.noreply.github.com>
2023-02-20 21:06:32 +0300

    Weather app, without error handler

--- js/weather.js
+++ js/weather.js
@@ -5,14 +5,20 @@ const selectedCity = document.querySelector('.Main-space_container-one_weather_l
 const selectedCityTemp = document.querySelector('.Main-space_container-one_weather_degrees');
 const selectedCityWeather = document.querySelector('.Main-space_container-one_weather_pic');
 
-/* searchButton.addEventListener('click', weather(searchLocationInput.value));
-searchButton.addEventListener('submit', );
- */
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    weather(searchLocationInput.value)
+});
+
+searchButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    weather(searchLocationInput.value)
+})
 
 
 function weather(cityName) {
 
-    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const serverUrl = 'http://api.openweathermap.org1/data/2.5/weather';
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
     const url = (`${serverUrl}?q=${cityName}&appid=${apiKey}`);
 
@@ -25,9 +31,19 @@ function weather(cityName) {
         .then((data) => {
             selectedCity.textContent = data.name;
             selectedCityTemp.innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-            selectedCityWeather.innerHTML = `<img src ="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+            selectedCityWeather.src = (`http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`);
+        })
+
+        .catch(err=>{
+           alert(err);
         })
 }
 
 
-weather('Moscow');
+
+
+
+
+
+
+/* weather('Nalchik'); */
