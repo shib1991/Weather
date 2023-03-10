0eddd81336b50a8d1a5c712c547c92abd9d3cd76
shib1991 <54975465+shib1991@users.noreply.github.com>
2023-03-10 22:10:55 +0300

    Corrected the key fot localstorage in the module

--- js/module.js
+++ js/module.js
@@ -1,21 +1,23 @@
+const KEY = 'savedLocation'
+
 function saveInStorage(array) {
-    return localStorage.setItem('savedLocation', JSON.stringify(array));
+    return localStorage.setItem(KEY, JSON.stringify(array));
 }
 
 function FavoriteCities(farray) {
-    const array = JSON.parse(localStorage.savedLocation);
+    const array = JSON.parse(localStorage.getItem(KEY));
     farray = farray.concat(array);
     return farray;
 }
 
 function showFavoriteCities() {
 
-    const favarray = JSON.parse(localStorage.savedLocation);
+    const favarray = JSON.parse(localStorage.getItem(KEY));
     return favarray;
 }
 
 function showLastLocation() {
-    const array = JSON.parse(localStorage.savedLocation);
+    const array = JSON.parse(localStorage.getItem(KEY));
     let currentC = array[array.length - 1];
     return currentC;
 }
